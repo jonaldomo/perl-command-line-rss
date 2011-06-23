a6236f3081184fc14e5fda97f151b84af805b885
John Moses <moses.john.r@gmail.com>
2011-06-23 08:49:51 -0500

    updating rss.pl to use menu, execute using lynx

--- rss.pl
+++ rss.pl
@@ -1,23 +1,38 @@
 use XML::RSS::Parser::Lite;
 use LWP::Simple;
-no warnings;
+use Cwd;
+use strict;
+binmode STDOUT, ":utf8";
 
-        
-my $xml = get($ARGV[0]) || die("Unable to load RSS feed!");
-my $rp = new XML::RSS::Parser::Lite;
-$rp->parse($xml);
-@links = qw//;
-        
-print $rp->get('title') . " \n" . $rp->get('url') . "\n" . $rp->get('description') . "\n\n";
+if($ARGV[0] =~ /-h/){
+    my $dir = getdcwd();
+    print "perl-command-line-rss"
+         ."\nView rss feeds and articles from the command prompt"
+         ."\nUsage: $dir\\rss.pl [option] [rss feed]"
+         ."\n\t-h\t\tthis help menu"
+         ."\n\t-o\t\topen rss feed.  e.g. rss.pl -o http://news.ycombinator.org/rss"
+         ."\n\t-v\t\tstored rss feeds";
+    exit;
+} 
 
-for (my $i = 0; $i < $rp->count(); $i++) {
-   
-    my $it = $rp->get($i);
-    $links[$i] = $it->get('url');
-    print "$i) ".$it->get('title')."\n"; # . " " . $it->get('url') . " " . $it->get('description') . 
+if($ARGV[0] =~ /-o/){     
+    my $xml = get($ARGV[1]) || die("Unable to load RSS feed!");
+    my $rp = new XML::RSS::Parser::Lite;
+    $rp->parse($xml);
+    my @links = qw//;
+            
+    print $rp->get('title') . " \n" . $rp->get('url') . "\n" . $rp->get('description') . "\n\n";
+
+    for (my $i = 0; $i < $rp->count(); $i++) {
+       
+        my $it = $rp->get($i);
+        $links[$i] = $it->get('url');
+        print "$i) ".$it->get('title')."\n"; # . " " . $it->get('url') . " " . $it->get('description') . 
+    }
+
+    print "\nWhich article # would you like to view? ";
+    my $article = <STDIN>;
+    print "\nOpening url $links[$article]\n";
+
+    exec("C:\\lynx_w32\\lynx -accept_all_cookies $links[$article]") || die("Unable to open $links[$article]");
 }
-
-print "\nWhich article # would you like to view? ";
-$article = <STDIN>;
-print "\nOpening url $links[$article]\n";
-exec("C:\\lynx_w32\\lynx -accept_all_cookies $links[$article]") || die("Unable to open $links[$article]");
